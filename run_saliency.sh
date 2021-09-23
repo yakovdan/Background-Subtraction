2178bbde2cd0f472df523686ca6095552769e73b
Yakov Danishevsky <yakovdan@gmail.com>
2021-09-24 01:54:38 +0300

    scripts

--- run_saliency.sh
+++ run_saliency.sh
@@ -3,3 +3,4 @@ git clone https://github.com/yakovdan/Datasets.git
 cd /home/yakovdan/Background-Subtraction/
 python3 ./computeRPCADecomposition.py --input /home/yakovdan/repos/Datasets/... --output ./output/smallscale/ --frame_start 0 --frame_end 862 --downscale 4
 python3 ./computeRPCADecomposition.py --input /home/yakovdan/repos/Datasets/... --output ./output/fullscale/ --frame_start 0 --frame_end 862 --downscale 1
+echo ok
