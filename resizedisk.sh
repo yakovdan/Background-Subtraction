1ab158652a21772fd22a95d7b576a32fbf6fa36b
Yakov Danishevsky <yakovdan@gmail.com>
2021-09-24 02:01:29 +0300

    fixed script

--- resizedisk.sh
+++ resizedisk.sh
@@ -1,3 +1,3 @@
-$ sudo apt install -y cloud-guest-utils   # Debian stretch, Ubuntu
-$ sudo growpart /dev/sda 1
-$ sudo resize2fs /dev/sda1
+sudo apt install -y cloud-guest-utils   # Debian stretch, Ubuntu
+sudo growpart /dev/sda 1
+sudo resize2fs /dev/sda1
