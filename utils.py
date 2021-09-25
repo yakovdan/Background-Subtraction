ca857634b1db0a1844c26e6e9501bddb46299ecd
Yakov Danishevsky <yakovdan@gmail.com>
2021-09-25 21:02:29 +0300

    added misc supporting functions to utils

--- utils.py
+++ utils.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 import os
 import shutil
 import glob
+
 from functools import reduce
 from numpy import linalg as LA
 from scipy.sparse.linalg import svds
 from datetime import datetime
+import networkx as nx
 
 
 def create_folder(name):
@@ -55,7 +57,7 @@ def bitmap_to_mat(bitmap_seq, grayscale=True):
     return matrix
 
 
-def import_video_as_frames(path, start, end):
+def import_video_as_frames(path, start, end, file_ending="jpg", work_type=np.float64):
     """
     import video
     using dtype=np.float64 to allow normalizing. use np.uint8 if not needed.
@@ -65,10 +67,10 @@ def import_video_as_frames(path, start, end):
     ImData0 is in [h,w, t] order and is a fortran array
 
     """
-    frames_list = glob.glob(path+'*.jpg')
+    frames_list = glob.glob(path + '*.'+file_ending)
     frames_list.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
     frames_list = frames_list[start:end]
-    bitmap_seq = bitmap_to_mat(frames_list, grayscale=True).astype(np.float64)
+    bitmap_seq = bitmap_to_mat(frames_list, grayscale=True).astype(work_type)
     video_data = np.ascontiguousarray(bitmap_seq.transpose((2, 0, 1)))
     ImData0 = np.asfortranarray(bitmap_seq)
     return ImData0, video_data
@@ -81,7 +83,7 @@ def save_images(image_array, path, grayscale):
     """
 
     num_images = image_array.shape[0]
-    filenames = [path+f"/output_image{i}.bmp" for i in range(num_images)]
+    filenames = [path + f"/output_image{i}.bmp" for i in range(num_images)]
     for i in range(num_images):
         if grayscale:
             cv2.imwrite(filenames[i], image_array[i, :, :])
@@ -90,7 +92,7 @@ def save_images(image_array, path, grayscale):
 
 
 def plot_errors(errors_list, filename, display=False, log_scale=True):
-    iterations = np.arange(1, len(errors_list)+1)
+    iterations = np.arange(1, len(errors_list) + 1)
     if log_scale:
         data = np.log(np.array(errors_list))
     else:
@@ -107,7 +109,7 @@ def plot_errors(errors_list, filename, display=False, log_scale=True):
 def resize_with_cv2_timefirst(images, ratio):
     result_size = [int(np.ceil(images.shape[i] * ratio)) for i in [1, 2]]
     T = images.shape[0]
-    result = np.empty([T]+result_size)
+    result = np.empty([T] + result_size)
     interpolation = cv2.INTER_AREA if ratio < 1 else cv2.INTER_CUBIC
     for t in range(T):
         result[t, :, :] = cv2.resize(images[t, :, :], result_size[::-1], interpolation=interpolation)
@@ -158,6 +160,7 @@ def save_mat_to_bin(matrix, filename):
     f = open(filename, 'wb')
     f.write(matrix.tobytes())
     f.close()
+
 
 def maxWithIdx(l):
     max_idx = np.argmax(l)
@@ -221,24 +224,24 @@ def normalizeSparseMat(image):
     binary_image = np.zeros_like(new_image)
     mask_mean = np.mean(new_image)
     mask_std = np.std(new_image)
-    binary_image[new_image > mask_mean + 2*mask_std] = 1.0
+    binary_image[new_image > mask_mean + 2 * mask_std] = 1.0
     return binary_image
 
 
-def output_result_bitmap_seq(folder_name, data,  lowrank_recon, sparse_mask, sparse_mask_bin):
+def output_result_bitmap_seq(folder_name, data, lowrank_recon, sparse_mask, sparse_mask_bin):
     """
     This function takes a folder name and write frames to it
     where each frame is a concatenation of a data frame, low rank frame, sparse frame and sparse mask frame
     """
-    video_data = np.zeros((data.shape[0], 4*data.shape[1], data.shape[2]), dtype=np.uint8)
+    video_data = np.zeros((data.shape[0], 4 * data.shape[1], data.shape[2]), dtype=np.uint8)
     for i in range(data.shape[2]):
-        data_out = (data[:, :, i]*255).astype(np.uint8)
-        lowrank_out = (lowrank_recon[:, :, i]*255).astype(np.uint8)
-        sparse_out = (sparse_mask[:, :, i]*255).astype(np.uint8)
-        sparse_bin_out = (sparse_mask_bin[:, :, i]*255).astype(np.uint8)
+        data_out = (data[:, :, i] * 255).astype(np.uint8)
+        lowrank_out = (lowrank_recon[:, :, i] * 255).astype(np.uint8)
+        sparse_out = (sparse_mask[:, :, i] * 255).astype(np.uint8)
+        sparse_bin_out = sparse_mask_bin[:, :, i].astype(np.uint8) * 255
         output_frame = np.concatenate((data_out, lowrank_out, sparse_out, sparse_bin_out), axis=1)
         video_data[:, :, i] = output_frame
-        cv2.imwrite(folder_name+f"frame_{i}.bmp", output_frame)
+        cv2.imwrite(folder_name + f"frame_{i}.bmp", output_frame)
 
 
 def write_log_to_file(filename, args):
@@ -247,3 +250,154 @@ def write_log_to_file(filename, args):
         logfile.write(f"Starting computation at {now}\n")
         for key, value in vars(args).items():
             logfile.write(f"{key} : {value}\n")
+
+
+def parse_numerical_values(path):
+    with open(path, 'r') as vals_file:
+        line = vals_file.read()
+        params = line.split(",")
+        first_val_str = params[0].split(":")[1]
+        second_string = " ".join(params[1:])
+        second_params = second_string.split(":")
+        second_val_str = second_params[1]
+        video_mean = float(first_val_str)
+        second_val_str = second_val_str.lstrip()
+        second_val_str = second_val_str.rstrip()
+        second_val_str = second_val_str[1:-1]
+        second_val_Lst = second_val_str.split(" ")
+        int_vals = [int(x) for x in second_val_Lst if len(x) > 0]
+        original_downsampled_shape = tuple(int_vals)
+    return video_mean, original_downsampled_shape
+
+
+def show_sequence_on_screen(np_array, use_abs=False, video_mean=0, indices=None):
+    min_val = np.min(np_array)
+    max_val = np.max(np_array)
+    shape = np_array.shape
+    if indices is None:
+        indices = list(range(0, shape[2], shape[2] // 10))
+    for i in indices:
+        if use_abs:
+            frame = np.abs(np_array[:, :, i])
+        else:
+            frame = np_array[:, :, i]
+
+        cv2.imwrite('test.bmp', (255 * (frame + video_mean)).astype(np.uint8))
+        cv2.imshow('OnScreen', frame + video_mean)
+        cv2.waitKey(0)
+
+
+def contained_in(cc1, cc2):
+    """
+        checks if the rectangle [x1,y1,w1,h1] is contained in [x2,y2,w2,h2]
+    """
+    x2, y2, w2, h2 = cc2
+    x1, y1, w1, h1 = cc1
+    if x2 < x1 and y2 < y1 and x1 + w1 < x2 + w2 and y1 + h1 < y2 + h2:
+        return True
+
+    return False
+
+
+def unite_nestedCCs(num_labels, labels, stats):
+    """
+    given a number of labels, the labels np array and stats from cv2.connectedcomponents
+    this function returns a new labels array and a dictionary of labels and areas
+    such that all nested CCs have the same label and count towards the same area
+
+    """
+
+    cc_dict = {}
+    nested_cc_list = []  # (1, 2) is in nested_cc_list iff cc 1's bbox is contained in cc 2's bbox
+    for i in range(0, num_labels):
+
+        # i ==0 is the background cc by convention. ignore
+        if i == 0:
+            continue
+
+        # extract stats per cc
+        x = stats[i, cv2.CC_STAT_LEFT]
+        y = stats[i, cv2.CC_STAT_TOP]
+        w = stats[i, cv2.CC_STAT_WIDTH]
+        h = stats[i, cv2.CC_STAT_HEIGHT]
+        area = stats[i, cv2.CC_STAT_AREA]
+        cc_dict[i] = ((x, y, w, h), area)
+
+    for label1, stats1 in cc_dict.items():
+        for label2, stats2 in cc_dict.items():
+            if label1 == label2:
+                continue
+            if contained_in(stats1[0], stats2[0]):
+                nested_cc_list.append((label1, label2))
+
+    nested_reverse_cc_list = [(x[1], x[0]) for x in nested_cc_list]
+    graph = nx.Graph()
+
+    for n1, n2 in nested_reverse_cc_list:
+        graph.add_edge(n1, n2)
+
+    spanning_tree = nx.minimum_spanning_tree(graph)
+    edges = spanning_tree.edges()
+    new_labels = np.copy(labels)
+
+    for n1, n2 in edges:
+        new_labels[labels == n2] = n1
+
+    unique, counts = np.unique(new_labels, return_counts=True)
+    ccs_dict = dict(zip(unique, counts))
+    del ccs_dict[0]
+    return ccs_dict, new_labels
+
+
+def filter_sparse_map(sparse_array, size_thresh=None):
+    """
+    This function takes a binary sparse array
+    and filters it such that no object smaller than size_thresh remains
+    """
+    connectivity = 4  # choose 4 or 8 for connectivity type
+    if size_thresh is None:
+        size_thresh = (sparse_array.shape[0] * sparse_array.shape[1]) // 1500  # from paper
+    result_sparse_array = np.zeros_like(sparse_array)
+    for i in range(sparse_array.shape[2]):
+        num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(sparse_array[:, :, i].astype(np.uint8)*255,
+                                                                                connectivity, cv2.CV_32S)
+        ccs_dict, new_labels = unite_nestedCCs(num_labels, labels, stats)
+        for label, area in ccs_dict.items():
+            if area > size_thresh:
+                result_sparse_array[:, :, i][new_labels == label] = True
+    return result_sparse_array
+
+
+def find_bounding_area_in_frame(sparse_frame):
+    connectivity = 4  # choose 4 or 8 for connectivity type
+    work_frame = np.copy(sparse_frame)
+    if work_frame.dtype != np.uint8:
+        work_frame = work_frame.astype(np.uint8)
+
+    work_frame[work_frame != 0] = 255
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(work_frame, connectivity, cv2.CV_32S)
+    cc_dimensions = []
+    for i in range(0, num_labels):
+        # i ==0 is the background cc by convention. ignore
+        if i == 0:
+            continue
+        cc_dimensions.append((stats[i, cv2.CC_STAT_LEFT],
+                              stats[i, cv2.CC_STAT_TOP],
+                              stats[i, cv2.CC_STAT_WIDTH]+stats[i, cv2.CC_STAT_LEFT],
+                              stats[i, cv2.CC_STAT_HEIGHT]+stats[i, cv2.CC_STAT_TOP]))
+
+    min_left = cc_dimensions.sort(key = lambda x: x[0])[0]
+    min_top = cc_dimensions.sort(key = lambda x: x[1])[0]
+    max_right = cc_dimensions.sort(key = lambda x: x[2])[-1]
+    max_bottom = cc_dimensions.sort(key = lambda x: x[3])[-1]
+    return min_left, min_top, max_right, max_bottom
+
+
+def find_bounding_volume(sparse_mat):
+    bounding_boxes = [find_bounding_area_in_frame(sparse_mat[:, :, i]) for i in sparse_mat.shape[2]]
+    min_left = bounding_boxes.sort(key = lambda x: x[0])[0]
+    min_top = bounding_boxes.sort(key = lambda x: x[1])[0]
+    max_right = bounding_boxes.sort(key = lambda x: x[2])[-1]
+    max_bottom = bounding_boxes.sort(key = lambda x: x[3])[-1]
+    return (min_left, min_top, max_right, max_bottom)
+    
