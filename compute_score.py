43755879aaabcc5065d136af92d104a35542331e
Yakov Danishevsky <yakovdan@gmail.com>
2021-09-30 19:39:05 +0300

    bug fix

--- compute_score.py
+++ compute_score.py
@@ -6,12 +6,12 @@ known_values = [0, 50, 255]  #  from CDNET 2014
 
 
 def create_pretty_score_map(sparse_mat, gt_mat, roi_mask):
-    roi_mask_bool= roi_mask == 255
+    roi_mask_bool =  roi_mask == 255
     roi_mask_bool_3d = np.stack([roi_mask_bool] * min(gt_mat.shape[2],sparse_mat.shape[2]) , axis=2)
     pretty_map = np.zeros((list(sparse_mat.shape)+[3]), dtype=np.uint8)
     map_tp = np.logical_and(np.logical_and(sparse_mat, gt_mat), roi_mask_bool_3d)
-    map_fp = np.logical_and(np.logical_and(sparse_mat, np.logical_not(gt_mat)), roi_mask_bool)
-    map_fn = np.logical_and(np.logical_and(np.logical_not(sparse_mat), gt_mat), roi_mask_bool)
+    map_fp = np.logical_and(np.logical_and(sparse_mat, np.logical_not(gt_mat)), roi_mask_bool_3d)
+    map_fn = np.logical_and(np.logical_and(np.logical_not(sparse_mat), gt_mat), roi_mask_bool_3d)
     # place a white pixel for TP
     pretty_map[map_tp, :] = np.array((255, 255, 255), dtype=np.uint8)
     # place a red pixel for FN
